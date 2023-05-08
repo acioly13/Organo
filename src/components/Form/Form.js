25f16da33a5723bf9178dc63cfc430d96fd0d60d
João Acioly <joao.acioly@dmslog.com>
2023-05-08 09:33:51 -0300

    evento digitar

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,10 +1,17 @@
 import './Form.css'
 
 const Form = (props) => {
+
+    let valor = '';
+    const aoDigitado = (event) => {
+        valor = event.target.value;
+        console.log(valor)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
